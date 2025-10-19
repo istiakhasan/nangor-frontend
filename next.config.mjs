cd106389596a275674900ff41ef3c4be36e31089
Istiak Hasan <istiakhasan1996@gmail.com>
2025-10-19 18:25:08 +0600

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["i.ibb.co"], // ✅ whitelist your image host
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.ibb.co",
+      },
+    ],
   },
 };
 
-export default nextConfig; // ✅ use export default instead of module.exports
+export default nextConfig;
